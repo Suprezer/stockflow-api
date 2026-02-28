28920b1afad2d81dfbfb3ece14c97622566a784b
Jonas M. Olesen <jmo@live.dk>
2026-02-28 23:14:36 +0100

    Create UsersController.cs

---
+++ stockflow-api/stockflow-api/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace stockflow_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            // Placeholder for getting users logic
+            return Ok(new[] { "User1", "User2", "User3" });
+        }
+    }
+}
