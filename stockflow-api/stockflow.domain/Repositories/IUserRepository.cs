2ad26b2ba4467de78584b91f032b72b660d801f9
Jonas M. Olesen <jmo@live.dk>
2026-02-28 23:18:51 +0100

    Create IUserRepository.cs

---
+++ stockflow-api/stockflow.domain/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using StockFlow.Domain.Entities;
+
+namespace stockflow.domain.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<IEnumerable<User>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task AddAsync(User user, CancellationToken cancellationToken = default);
+        Task UpdateAsync(User user, CancellationToken cancellationToken = default);
+        Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+    }
+}
