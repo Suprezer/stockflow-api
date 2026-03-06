bc65543b942acf7859660b359ff3a3ae434ae37d
Jonas M. Olesen <jmo@live.dk>
2026-03-06 19:50:32 +0100

    Setup for datacontext for EF Core

---
+++ stockflow-api/stockflow.infrastructure/Data/StockFlowContext.cs
@@ -0,0 +1,15 @@
+﻿using stockflow.domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace stockflow.infrastructure.Data
+{
+    public class StockFlowContext : DbContext
+    {
+        public StockFlow(DbContextOptions<LibraryContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
