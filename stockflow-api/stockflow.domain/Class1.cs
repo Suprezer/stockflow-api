7baad69e420072f6d990c91d2673b71c59b68be6
Jonas M. Olesen <jmo@live.dk>
2026-03-07 23:59:28 +0100

    Addition of IProductRepository

--- stockflow-api/stockflow.domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace stockflow.domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ stockflow-api/stockflow.domain/Repositories/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using stockflow.domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stockflow.domain.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<Product?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Product>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task AddAsync(Product product, CancellationToken cancellationToken = default);
+        Task UpdateAsync(Product product, CancellationToken cancellationToken = default);
+        Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken = default);
+    }
+}
