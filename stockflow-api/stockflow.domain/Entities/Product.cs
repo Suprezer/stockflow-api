7d4f33bf91710b88baff32f6bb628e141af3d9de
Jonas M. Olesen <jmo@live.dk>
2026-03-08 23:59:23 +0100

    Expanded product

--- stockflow-api/stockflow.domain/Entities/Product.cs
+++ stockflow-api/stockflow.domain/Entities/Product.cs
@@ -5,6 +5,10 @@
         public Guid Id { get; private set; }
         public string Name { get; private set; }
         public string Description { get; private set; }
+        public decimal Price { get; private set; }
+
+        public DateTime CreatedAt { get; private set; }
+        public DateTime UpdatedAt { get; private set; }
 
         private Product() { }
 
@@ -13,6 +17,24 @@
             Id = Guid.NewGuid();
             Name = name.Trim();
             Description = description.Trim();
+        }
+
+        public void Rename(string newName)
+        {
+            Name = newName.Trim();
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void UpdateDescription(string newDescription)
+        {
+            Description = newDescription.Trim();
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void UpdatePrice(decimal newPrice)
+        {
+            Price = newPrice;
+            UpdatedAt = DateTime.UtcNow;
         }
     }
 }
