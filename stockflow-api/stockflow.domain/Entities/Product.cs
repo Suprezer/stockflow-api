37d07758ba1f1ad1af7b85b130ea9996a573aac1
Jonas M. Olesen <jmo@live.dk>
2026-03-10 00:00:12 +0100

    Update Product.cs

--- stockflow-api/stockflow.domain/Entities/Product.cs
+++ stockflow-api/stockflow.domain/Entities/Product.cs
@@ -6,6 +6,7 @@
         public string Name { get; private set; }
         public string Description { get; private set; }
         public decimal Price { get; private set; }
+        public int Quantity { get; private set; }
 
         public DateTime CreatedAt { get; private set; }
         public DateTime UpdatedAt { get; private set; }
