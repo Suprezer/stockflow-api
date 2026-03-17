afcd9b888cbbbd465fe5509607da36e204c237ff
Jonas M. Olesen <jmo@live.dk>
2026-03-17 23:59:17 +0100

    Added interface for StorageLocation repository

---
+++ stockflow-api/stockflow.domain/Repositories/IStorageLocation.cs
@@ -0,0 +1,17 @@
+﻿using stockflow.domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stockflow.domain.Repositories
+{
+    public interface IStorageLocation
+    {
+        Task<StorageLocation?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<IEnumerable<StorageLocation>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task AddAsync(StorageLocation location, CancellationToken cancellationToken = default);
+        Task UpdateAsync(StorageLocation location, CancellationToken cancellationToken = default);
+        Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken = default);
+    }
+}
