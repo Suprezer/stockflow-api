f847e177aa2d55d90a79735b071676fd91c43942
Jonas M. Olesen <jmo@live.dk>
2026-03-25 00:01:52 +0100

    Create ProductRepository.cs

---
+++ stockflow-api/stockflow.infrastructure/Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,41 @@
+﻿using stockflow.domain.Entities;
+using stockflow.domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stockflow.infrastructure.Persistence.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task AddAsync(Product product, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Product product, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
