955d482b7e3dea3d374c7b9b5af6273e7230dcbc
Jonas M. Olesen <jmo@live.dk>
2026-03-24 23:58:14 +0100

    Create UserReopsitory.cs

---
+++ stockflow-api/stockflow.infrastructure/Persistence/Repositories/UserReopsitory.cs
@@ -0,0 +1,36 @@
+﻿using stockflow.domain.Repositories;
+using StockFlow.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stockflow.infrastructure.Persistence.Repositories
+{
+    public class UserReopsitory : IUserRepository
+    {
+        public Task AddAsync(User user, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(User user, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
