cd5ecbce6dfccb5a15f8e7c961af71b2c67e1678
Jonas M. Olesen <jmo@live.dk>
2026-03-29 23:54:58 +0200

    Create StorageLocationRepository.cs

---
+++ stockflow-api/stockflow.infrastructure/Persistence/Repositories/StorageLocationRepository.cs
@@ -0,0 +1,11 @@
+﻿using stockflow.domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stockflow.infrastructure.Persistence.Repositories
+{
+    public class StorageLocationRepository : IStorageLocationRepository
+    {
+    }
+}
