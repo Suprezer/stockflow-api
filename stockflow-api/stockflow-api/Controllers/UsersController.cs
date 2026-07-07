c503d4eec38d7d35845a4145c7777907f612fb72
Jonas M. Olesen <jmo@live.dk>
2026-07-07 23:58:16 +0200

    Additions to UsersController

--- stockflow-api/stockflow-api/Controllers/UsersController.cs
+++ stockflow-api/stockflow-api/Controllers/UsersController.cs
@@ -12,5 +12,26 @@ namespace stockflow_api.Controllers
             // Placeholder for fetching all users
             return Ok(new[] { "User1", "User2", "User3" });
         }
+
+        [HttpGet]
+        public IActionResult GetUserById(int id)
+        {
+            // Placeholder for fetching a user by ID
+            return Ok($"User{id}");
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] string user)
+        {
+            // Placeholder for creating a new user
+            return CreatedAtAction(nameof(GetUserById), new { id = 1 }, user);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateUser(int id, [FromBody] string user)
+        {
+            // Placeholder for updating a user
+            return NoContent();
+        }
     }
 }
